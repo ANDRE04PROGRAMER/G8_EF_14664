046be599f9182ca3ab15df302679e5339d892a3c
Jair751 <USER@DESKTOP-5MLK96Q>
2026-04-04 17:45:36 -0500

    Se agrego clases

---
+++ EF/src/clases/Producto.java
@@ -0,0 +1,33 @@
+package clases;
+
+public class Producto {
+	int codigo, stock;
+	String nombre;
+	Double precio;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	
+	
+}
