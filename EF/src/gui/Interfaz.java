43a6487a934cc0de5377df1086d0159996cc8916
ANDRE04PROGRAMER <andre.alarcon.2004@gmail.com>
2026-04-10 11:34:40 -0500

    Interfaz

--- EF/src/gui/Interfaz.java
+++ EF/src/gui/Interfaz.java
@@ -6,11 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class Interfaz extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private JTextArea txtS;
+	private JButton btnBuscar;
+	private JButton btnAdicionar;
+	private JButton btnReportar;
+	private JTextField txtUNO;
+	private JTextField txtDOS;
 
 	/**
 	 * Launch the application.
@@ -32,12 +40,39 @@ public class Interfaz extends JFrame {
 	 * Create the frame.
 	 */
 	public Interfaz() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
+		getContentPane().setLayout(null);
+		{
+			txtS = new JTextArea();
+			txtS.setBounds(10, 108, 416, 171);
+			getContentPane().add(txtS);
+		}
+		{
+			btnBuscar = new JButton("Buscar");
+			btnBuscar.setBounds(132, 78, 84, 20);
+			getContentPane().add(btnBuscar);
+		}
+		{
+			btnAdicionar = new JButton("Adicionar");
+			btnAdicionar.setBounds(132, 53, 84, 20);
+			getContentPane().add(btnAdicionar);
+		}
+		{
+			btnReportar = new JButton("Reportar");
+			btnReportar.setBounds(132, 23, 84, 20);
+			getContentPane().add(btnReportar);
+		}
+		{
+			txtUNO = new JTextField();
+			txtUNO.setBounds(26, 24, 96, 18);
+			getContentPane().add(txtUNO);
+			txtUNO.setColumns(10);
+		}
+		{
+			txtDOS = new JTextField();
+			txtDOS.setBounds(26, 54, 96, 18);
+			getContentPane().add(txtDOS);
+			txtDOS.setColumns(10);
+		}
 
 	}
-
 }
